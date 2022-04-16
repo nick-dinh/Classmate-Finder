7bfb996a6a5dfb1f94ddd058e947ccc99abf1cd3
Nicholas Dinh <80604823+nick-dinh@users.noreply.github.com>
2022-04-16 17:54:54 -0400

    added posts & comments table schemas

--- database/database.sql
+++ database/database.sql
@@ -14,3 +14,24 @@ CREATE TABLE `chatrooms` (
   `users` varchar(64) NOT NULL DEFAULT '',
   PRIMARY KEY (`chatid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `post` (
+  `postid` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `author` varchar(16) NOT NULL DEFAULT '',
+  `title` varchar(16) NOT NULL DEFAULT '',
+  `body` text,
+  `timecreated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `scheduledtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`postid`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `comments` (
+  `commentid` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(16) NOT NULL DEFAULT '',
+  `message` text,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `post_id` int(11) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`commentid`),
+  KEY `fk_post_id` (`post_id`),
+  CONSTRAINT `fk_post_id` FOREIGN KEY (`post_id`) REFERENCES `posts` (`postid`)
+) ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=latin1;
