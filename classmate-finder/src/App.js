2aee908b9521275f0cf60a32b6356659441e220f
Nicholas Dinh <80604823+nick-dinh@users.noreply.github.com>
2022-04-16 17:56:57 -0400

    added posts & comments routes

--- classmate-finder/src/App.js
+++ classmate-finder/src/App.js
@@ -3,7 +3,11 @@ import {BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css'
 import CreateRoom from './pages/CreateRoom';
 import GetRooms from './pages/GetRooms';
-import Room from './pages/Room'
+import Room from './pages/Room';
+import GetPosts from './pages/GetPosts';
+import Post from './pages/Post';
+import CreatePost from './pages/CreatePost';
+import UpdatePost from './pages/UpdatePost';
 
 const App = () => {
   let [usr, setUsr] = useState("")
@@ -25,6 +29,10 @@ const App = () => {
         <Route path="/rooms/:userName" exact render={(props) => <GetRooms />} />
         <Route path="/createroom" render={(props)=> <CreateRoom />} />
         <Route path="/room/:roomId" render={(props)=> <Room />}/>
+        <Route path="/posts/" exact render={(props)=> <GetPosts />} />
+        <Route path="/post/:postId" exact render={(props)=> <Post />} />
+        <Route path="/createpost/" exact render={(props)=> <CreatePost />} />
+        <Route path="/updatepost/:postId" exact render={(props)=> <UpdatePost />} />
       </Router>
     </div>
   );
